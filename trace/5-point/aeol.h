26a40993e51988f42039ca65ec1f10dbcfeabe9e
dongchen-coder <jameschennerd@gmail.com>
2016-11-09 09:55:06 -0500

    add aeol alg 2

---
+++ trace/5-point/aeol.h
@@ -0,0 +1,207 @@
+#include <stdlib.h>
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+using namespace std;
+
+std::map<uint64_t, std::map<uint64_t, uint64_t> > f;
+std::map<uint64_t, std::map<uint64_t, uint64_t> > e;
+std::map<uint64_t, std::map<uint64_t, uint64_t> > rt;
+std::map<uint64_t, uint64_t> L;
+std::map<uint64_t, uint64_t> prevs;
+
+std::set<uint64_t> D;
+
+std::map<uint64_t, double> interRT;
+
+unsigned long ref_time = 0;
+
+std::map<uint64_t, double> nfactorial;
+uint64_t N = 0;
+
+void factorial(uint64_t n) {
+	nfactorial[0] = 1;
+	nfactorial[1] = 1;
+	for (uint64_t i = 2; i < n; i++) {
+		nfactorial[i] = nfactorial[i-1] * i; 
+	}
+	return;
+}
+
+void access(uint64_t addr, uint64_t wgid) {
+
+	ref_time++;		
+
+	if (f.find(wgid) == f.end()) {
+		if (prevs.empty() == false) {
+			for (std::map<uint64_t, uint64_t>::iterator it = prevs.begin(), eit = prevs.end(); it != eit; ++it) {
+				e[wgid-1][it->first] = ref_time - it->second;
+			}
+			L[wgid-1] = ref_time - 1;
+			prevs.clear();
+		}
+		ref_time = 1;
+		f[wgid][addr] = ref_time;
+		prevs[addr] = ref_time;
+		
+		if (N < wgid+1) {
+			N = wgid+1;
+		}
+
+	} else {
+		if (f[wgid].find(addr) == f[wgid].end()) {
+			f[wgid][addr] = ref_time;
+		} else {
+			rt[wgid][addr] = ref_time - prevs[addr];
+		}
+		prevs[addr] = ref_time;
+	}
+	
+	D.insert(addr);
+
+	return;
+}
+
+void reset() {
+	f.clear();
+	e.clear();
+	rt.clear();
+	prevs.clear();
+	
+	N = 0;
+	ref_time = 0;
+	interRT.clear();		
+	nfactorial.clear();
+	L.clear();
+	D.clear();
+	
+	return;
+}
+
+void aeol() {
+
+	std::map<uint64_t, std::vector<uint64_t>* > F;
+	std::map<uint64_t, std::vector<uint64_t>* > E;
+	std::map<uint64_t, uint64_t> M;
+	std::map<uint64_t, uint64_t> aveL;
+
+	/* finilize exit time calculation for the last work group */
+	for (std::map<uint64_t, uint64_t>::iterator it = prevs.begin(), eit = prevs.end(); it != eit; ++it) {
+    	e[N-1][it->first] = ref_time - it->second + 1;
+    }	
+	L[N-1] = ref_time;
+
+	/* init D, F, E, M*/
+	for (std::set<uint64_t>::iterator it = D.begin(), endit = D.end(); it != endit; ++it) {
+
+		M[*it] = 0;
+		aveL[*it] = 0;		
+
+		//cout << "N " << N << endl; 
+		for (int i = 0; i < N; i++) {
+			if (f[i].find(*it) == f[i].end()) {
+				aveL[*it] += L[i];
+				M[*it]++;
+			}
+		}
+		
+		if (M[*it] != 0) {
+			aveL[*it] = double(aveL[*it]) / M[*it];
+		}
+
+		//cout << *it << " avel " << aveL[*it] << " m " << M[*it] << endl;
+
+		F[*it] = new std::vector<uint64_t>();
+		for (std::map<uint64_t, std::map<uint64_t, uint64_t> >::iterator fit = f.begin(), efit = f.end(); fit != efit; ++ fit) {
+			if (fit->second.find(*it) != fit->second.end()) {
+				F[*it]->push_back(fit->second[*it]);
+			}
+		}
+
+		E[*it] = new std::vector<uint64_t>();
+		for (std::map<uint64_t, std::map<uint64_t, uint64_t> >::iterator eit = e.begin(), eeit = e.end(); eit != eeit; ++ eit) {
+			//cout << eit->first << " " << endl;
+			if (eit->second.find(*it) != eit->second.end()) {
+				E[*it]->push_back(eit->second[*it]);
+			}
+		}
+	}
+
+	/* alg 2 */	
+
+	factorial(N);
+
+	for (std::set<uint64_t>::iterator it = D.begin(), endit = D.end(); it != endit; ++it) {
+		//cout << "d " << *it << endl;
+		for (std::vector<uint64_t>::iterator fit = F[*it]->begin(), efit = F[*it]->end(); fit != efit; ++fit) {
+			//cout << "f " << *fit << " ";
+			for (std::vector<uint64_t>::iterator eit = E[*it]->begin(), eeit = E[*it]->end(); eit != eeit; ++eit) {
+				//cout << "e " << *eit << " ";
+				for (int i = 0; i <= M[*it]; i++) {
+					//cout << "m " << i << " avel " << aveL[*it] << " ";
+					uint64_t tmpRT = *eit + aveL[*it] * i + *fit - 1;
+					//cout << "rt " << tmpRT << endl;
+					if (interRT.find(tmpRT) != interRT.end()) {
+						interRT[tmpRT] += nfactorial[i] * nfactorial[N-i-1] * nfactorial[M[*it]] / nfactorial[i] / nfactorial[M[*it]-i];
+					} else {
+						interRT[tmpRT] = nfactorial[i] * nfactorial[N-i-1] * nfactorial[M[*it]] / nfactorial[i] / nfactorial[M[*it] - i];
+					}								
+				}
+			}
+		}	
+		for (int j = 0; j < N; j++) {
+			if (f[j].find(*it) != f[j].end()) {
+				for (int i = 0; i <= M[*it]; i++) {
+					uint64_t tmpRT = e[j][*it] + aveL[*it] * i + f[j][*it] - 1;
+					interRT[tmpRT] -= nfactorial[i] * nfactorial[N - i - 1] * nfactorial[M[*it]] / nfactorial[i] / nfactorial[M[*it] - i];
+				}
+			}
+		}
+	}	
+
+	return;
+}
+
+
+void dumpInterRT() {
+	for (std::map<uint64_t, double>::iterator it = interRT.begin(), eit = interRT.end(); it != eit; ++it) {
+		cout << it->first << "	" << it->second << endl;
+	}
+	return;
+}
+
+void dumpD() {
+	for (std::set<uint64_t>::iterator it = D.begin(), eit = D.end(); it != eit; ++it) {
+		cout << *it << " ";
+	}
+	cout << endl;
+	return;
+}
+
+void dumpf() {
+	for (std::map<uint64_t, std::map<uint64_t, uint64_t> >::iterator fit = f.begin(), efit = f.end(); fit != efit; ++ fit) {
+		cout << "work group " << fit->first << endl;
+		for (std::map<uint64_t, uint64_t>::iterator jt = fit->second.begin(), ejt = fit->second.end(); jt != ejt; ++jt) {
+			cout << "  d " << jt->first << " f " << jt->second << endl;
+		}
+	}
+	return;
+}
+
+void dumpe() {
+	for (std::map<uint64_t, std::map<uint64_t, uint64_t> >::iterator eit = e.begin(), eeit = e.end(); eit != eeit; ++ eit) {
+		cout << "work group " << eit->first << endl;
+		for (std::map<uint64_t, uint64_t>::iterator jt = eit->second.begin(), ejt = eit->second.end(); jt != ejt; ++jt) {
+			cout << "  d " << jt->first << " e " << jt->second << endl;
+		}
+	}
+	return;
+}
+
+void dumpL() {
+	for (std::map<uint64_t, uint64_t>::iterator it = L.begin(), eit = L.end(); it != eit; ++it) {
+		cout << " work group " << it->first << " l " << it->second << endl; 
+	}		
+	return;
+}	
---
+++ trace/5-point/stencil_aeol.cpp
@@ -0,0 +1,225 @@
+#include "aeol.h"
+
+
+void stencil(int * a, int * b, unsigned int dim_size, int isize, int jsize, int gxsize, int gysize)
+{
+      unsigned int size = dim_size + 2;
+      
+			for (int gidX = 0; gidX < gxsize; gidX++) {
+      	for (int gidY = 0; gidY < gysize; gidY++) {
+					unsigned int gidx = gidX * isize + 1;
+					unsigned int gidy = gidY * jsize + 1;
+					for (int i = 0; i < isize; i++) {
+						for (int j = 0; j < jsize; j++) {
+      				b[(gidx + i) * size + gidy + j] = a[(gidx+i) * size + gidy + j]
+																					+ a[(gidx+i) * size + gidy + 1 + j]
+																					+ a[(gidx+i) * size + gidy - 1 + j]
+																					+ a[(gidx +i- 1) * size + gidy + j]
+																					+ a[(gidx +i+ 1) * size + gidy + j];
+						}
+					}
+      	}
+			}
+			return;
+}
+
+void stencil_trace_GXYW(int *a, int *b, unsigned int dim_size, int gxsize, int gysize, int isize, int jsize) {
+	unsigned int size = dim_size + 2;
+	int wgsizeX = gxsize / 32;
+	int wgsizeY = gysize / 32;
+	for (int gidY = 0; gidY < wgsizeY; gidY++) {
+  	for (int gidX = 0; gidX < wgsizeX; gidX++) {
+  		for (int lidY = 0; lidY < 32; lidY++) {
+				int warpN = 32 / 16;
+				for (int warp = 0; warp < warpN; warp++) {
+          	unsigned int gidy = (gidY*32+lidY) * jsize + 1;
+						for (int i = 0; i < isize; i++) {
+          		for (int j = 0; j < jsize; j++) {
+								for (int w = 0; w < 16; w++) {
+									int lidX = 16 * warp + w;
+									unsigned int gidx = (gidX*32+lidX) * isize + 1;
+									//rtTmpAccess((gidx+i) * size + gidy + j);
+									access((gidx+i) * size + gidy + j, gidY * wgsizeX + gidX);
+								}
+								for (int w = 0; w < 16; w++) {
+          				int lidX = 16 * warp + w;
+                  unsigned int gidx = (gidX*32+lidX) * isize + 1;
+									//rtTmpAccess((gidx+i) * size + gidy + 1 + j);
+          				access((gidx+i) * size + gidy + 1 + j, gidY * wgsizeX + gidX);
+								}
+								for (int w = 0; w < 16; w++) {
+                  int lidX = 16 * warp + w;
+                  unsigned int gidx = (gidX*32+lidX) * isize + 1;
+									//rtTmpAccess((gidx+i) * size + gidy - 1 + j);
+          				access((gidx+i) * size + gidy - 1 + j, gidY * wgsizeX + gidX);
+								}
+								for (int w = 0; w < 16; w++) {
+                  int lidX = 16 * warp + w;
+                  unsigned int gidx = (gidX*32+lidX) * isize + 1;
+									//rtTmpAccess((gidx +i- 1) * size + gidy + j);
+									access((gidx +i- 1) * size + gidy + j, gidY * wgsizeX + gidX);
+								}
+								for (int w = 0; w < 16; w++) {
+                  int lidX = 16 * warp + w;
+                  unsigned int gidx = (gidX*32+lidX) * isize + 1;
+									//rtTmpAccess((gidx +i+ 1) * size + gidy + j);
+          				access((gidx +i+ 1) * size + gidy + j, gidY * wgsizeX + gidX);
+								}
+								for (int w = 0; w < 16; w++) {
+                  int lidX = 16 * warp + w;
+                  unsigned int gidx = (gidX*32+lidX) * isize + 1;
+									//rtTmpAccess((gidx + i) * size + gidy + j + 1026*1026);
+									access((gidx + i) * size + gidy + j + 1026*1026, gidY * wgsizeX + gidX);
+									b[(gidx + i) * size + gidy + j] = a[(gidx+i) * size + gidy + j] +
+											a[(gidx+i) * size + gidy + 1 + j] + a[(gidx+i) * size + gidy - 1 + j] +
+											a[(gidx +i- 1) * size + gidy + j] + a[(gidx +i+ 1) * size + gidy + j];
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	return;
+}
+
+void stencil_trace_GXYW_RR(int *a, int *b, unsigned int dim_size, int gxsize, int gysize, int isize, int jsize) {
+
+	unsigned int size = dim_size + 2;
+  int wgsizeX = gxsize / 32;
+  int wgsizeY = gysize / 32;
+  for (int gidY = 0; gidY < wgsizeY; gidY++) {
+    for (int gidX = 0; gidX < wgsizeX; gidX++) {
+      for (int i = 0; i < isize; i++) {
+      	for (int j = 0; j < jsize; j++) {
+					for (int lidY = 0; lidY < 32; lidY++) {
+        		for (int lidX = 0; lidX < 32; lidX++) {
+ 							unsigned int gidx = (gidX*32+lidX) * isize + 1;
+          		unsigned int gidy = (gidY*32+lidY) * jsize + 1;
+	           	//rtTmpAccess((gidx+i) * size + gidy + j);
+            	access((gidx+i) * size + gidy + j, gidY * wgsizeX + gidX);
+						}
+					}
+					for (int lidY = 0; lidY < 32; lidY++) {
+            for (int lidX = 0; lidX < 32; lidX++) {
+              unsigned int gidx = (gidX*32+lidX) * isize + 1;
+              unsigned int gidy = (gidY*32+lidY) * jsize + 1;
+							//rtTmpAccess((gidx+i) * size + gidy + 1 + j);
+							access((gidx+i) * size + gidy + 1 + j, gidY * wgsizeX + gidX);
+						}
+					}
+					for (int lidY = 0; lidY < 32; lidY++) {
+            for (int lidX = 0; lidX < 32; lidX++) {
+              unsigned int gidx = (gidX*32+lidX) * isize + 1;
+              unsigned int gidy = (gidY*32+lidY) * jsize + 1;
+            	//rtTmpAccess((gidx+i) * size + gidy - 1 + j);
+							access((gidx+i) * size + gidy - 1 + j, gidY * wgsizeX + gidX);
+						}
+					}
+					for (int lidY = 0; lidY < 32; lidY++) {
+            for (int lidX = 0; lidX < 32; lidX++) {
+              unsigned int gidx = (gidX*32+lidX) * isize + 1;
+              unsigned int gidy = (gidY*32+lidY) * jsize + 1;
+            	//rtTmpAccess((gidx +i- 1) * size + gidy + j);
+							access((gidx +i- 1) * size + gidy + j, gidY * wgsizeX + gidX);
+						}
+					}
+					for (int lidY = 0; lidY < 32; lidY++) {
+            for (int lidX = 0; lidX < 32; lidX++) {
+              unsigned int gidx = (gidX*32+lidX) * isize + 1;
+              unsigned int gidy = (gidY*32+lidY) * jsize + 1;
+            	//rtTmpAccess((gidx +i+ 1) * size + gidy + j);
+							access((gidx +i+ 1) * size + gidy + j, gidY * wgsizeX + gidX);
+						}
+					}
+					for (int lidY = 0; lidY < 32; lidY++) {
+            for (int lidX = 0; lidX < 32; lidX++) {
+              unsigned int gidx = (gidX*32+lidX) * isize + 1;
+              unsigned int gidy = (gidY*32+lidY) * jsize + 1;
+              //rtTmpAccess((gidx + i) * size + gidy + j + 1026*1026);
+							access((gidx + i) * size + gidy + j + 1026*1026, gidY * wgsizeX + gidX);
+							b[(gidx + i) * size + gidy + j] = a[(gidx+i) * size + gidy + j] +
+                      a[(gidx+i) * size + gidy + 1 + j] + a[(gidx+i) * size + gidy - 1 + j] +
+                      a[(gidx +i- 1) * size + gidy + j] + a[(gidx +i+ 1) * size + gidy + j];
+      	 		}
+					}
+				}
+			}
+		}	
+	}
+}
+
+bool varify(int * b, int * a) {
+	for (int i = 1; i < 1025; i++) {
+		for (int j = 1; j < 1025; j++) {
+			//if (b[i * 1026 + j] != a[i*1026+j] + a[i*1026+j + 1] + a[i*1026+j - 1] + a[(i-1)*1026+j] + a[(i+1)*1026+j]) {
+			//	return false;
+			//}
+			if (a[i * 1026 + j] != b[i * 1026 + j]) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+void testCase () {
+	access(1, 0);
+	access(2, 0);
+	access(3, 0);
+	access(1, 0);
+	access(2, 1);
+	access(3, 1);
+	access(1, 1);
+	access(1, 1);
+	access(2, 2);
+	access(3, 2);
+	access(2, 2);
+    access(3, 2);
+	return;
+}
+
+int main() {
+	int* a = (int*)malloc(1026*1026*sizeof(int));
+	int* b = (int*)malloc(1026*1026*sizeof(int));
+	int* b_r = (int*) malloc(1026*1026*sizeof(int));
+
+	for (int i = 0; i < 1026 * 1026; i++) {
+			a[i] = i % 256;
+	} 	
+
+
+	stencil(a, b_r, 1024, 1024, 1024, 1, 1);
+	
+	for (int i = 32; i <= 1024; i = i * 2) {
+		for (int j = 32; j <= 1024; j = j * 2) {
+			cout << i << " " << j << " --------" << endl;
+ 			stencil_trace_GXYW(a, b, 1024, i, j, 1024/i, 1024/j);		
+			
+			aeol();
+			//dumpD();
+			//dumpf();
+			//dumpe();
+			dumpInterRT();			
+
+			reset();
+		}
+	}
+	
+	/*
+	testCase();
+	aeol();
+	dumpD();
+	dumpL();
+	dumpf();
+	dumpe();
+    dumpInterRT();  	
+	reset();
+	*/
+	return 0;
+}
+
+
+
+
+

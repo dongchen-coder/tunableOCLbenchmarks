e310c4866c87ed0e734cdcbb9273578904a709c6
dongchen-coder <jameschennerd@gmail.com>
2017-01-09 18:13:37 -0500

    add mr profiling

---
+++ trace/mr/mr.c
@@ -0,0 +1,18 @@
+#include "../intraEOinsensitiveTrace/3dConvolution.h"
+
+#include "../../utilities/aeol.h"
+
+
+int main() {
+
+	void (*f1) (uint64_t, uint64_t) = access;
+    void (*f2) () = reset;
+    void (*f3) () = aeol;
+	void (*f4) () = dumpInterRT;
+
+	convolution3d_main(f1, f2, f3, f4);
+
+	return 0;
+}
+
+

d318a8e96e1ee98b694f2e15f6d6a0745ec8cf34
Dong Chen <dchen39@f04.cs.rochester.edu>
2017-02-09 15:19:28 -0500

    update aeol.h

--- utilities/aeol.h
+++ utilities/aeol.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <iostream>
+#include <unordered_map>
 #include <map>
 #include <set>
 #include <vector>
@@ -7,31 +8,46 @@
 using namespace std;
 
 /* raw data */
-std::map<uint64_t, std::map<uint64_t, uint64_t> > f;
-std::map<uint64_t, std::map<uint64_t, uint64_t> > e;
-std::map<uint64_t, std::map<uint64_t, uint64_t> > rt;
-std::map<uint64_t, uint64_t> L;
-std::map<uint64_t, uint64_t> prevs;
+std::unordered_map<uint64_t, std::unordered_map<uint64_t, uint64_t> > f;
+std::unordered_map<uint64_t, std::unordered_map<uint64_t, uint64_t> > e;
+std::unordered_map<uint64_t, std::unordered_map<uint64_t, uint64_t> > rt;
+std::unordered_map<uint64_t, uint64_t> L;
+std::unordered_map<uint64_t, uint64_t> prevs;
 std::set<uint64_t> D;
-std::map<uint64_t, double> interRT;
+std::unordered_map<uint64_t, double> interRT;
 
 uint64_t ref_time = 0;
+uint64_t trace_len = 0;
 
 uint64_t N = 0;
 
 /* processed data for alg 2 */
-std::map<uint64_t, std::vector<uint64_t>* > F;
-std::map<uint64_t, std::vector<uint64_t>* > E;
-std::map<uint64_t, uint64_t> M;
-std::map<uint64_t, uint64_t> aveL;
+std::unordered_map<uint64_t, std::vector<uint64_t>* > F;
+std::unordered_map<uint64_t, std::vector<uint64_t>* > E;
+std::unordered_map<uint64_t, uint64_t> M;
+std::unordered_map<uint64_t, uint64_t> aveL;
 
 /* processed data for alg 3 */
 uint64_t cid = 0;
-std::map<std::string, uint64_t> CID;
-std::map<uint64_t, std::map<uint64_t, uint64_t> > CEF;
-std::map<uint64_t, uint64_t> CM;
-std::map<uint64_t, uint64_t> CaveL;
+std::unordered_map<std::string, uint64_t> CID;
+std::unordered_map<uint64_t, std::unordered_map<uint64_t, uint64_t> > CEF;
+std::unordered_map<uint64_t, uint64_t> CM;
+std::unordered_map<uint64_t, uint64_t> CaveL;
 
+/* intra RT*/
+std::unordered_map<uint64_t, uint64_t> intraRT;
+
+/* aeol F */
+std::unordered_map<uint64_t, double> fRT;
+
+/* aeol E */
+std::unordered_map<uint64_t, double> eRT;
+
+/* merged RT */
+std::map<uint64_t, double> RT;
+
+/* FP */
+std::map<uint64_t, double> FP;
 
 #define CLASSIFICATION
 
@@ -75,13 +91,27 @@ inline uint64_t valueToBin(uint64_t value) {
     }
 }
 
+void intraRT_cal(uint64_t addr, uint64_t time) {
+
+    if (prevs.find(addr) != prevs.end()) {
+        if (intraRT.find(time - prevs[addr]) != intraRT.end()) {
+            intraRT[time - prevs[addr]] += 1;
+        } else {
+            intraRT[time - prevs[addr]] = 1;
+        }
+    }
+
+    return;
+}
+
 void access(uint64_t addr, uint64_t wgid) {
 
 	ref_time++;		
+	trace_len++;
 
 	if (f.find(wgid) == f.end()) {
 		if (prevs.empty() == false) {
-			for (std::map<uint64_t, uint64_t>::iterator it = prevs.begin(), eit = prevs.end(); it != eit; ++it) {
+			for (std::unordered_map<uint64_t, uint64_t>::iterator it = prevs.begin(), eit = prevs.end(); it != eit; ++it) {
 				e[wgid-1][it->first] = ref_time - it->second;
 			}
 			L[wgid-1] = ref_time - 1;
@@ -89,6 +119,8 @@ void access(uint64_t addr, uint64_t wgid) {
 		}
 		ref_time = 1;
 		f[wgid][addr] = ref_time;
+		
+		intraRT_cal(addr, ref_time);
 		prevs[addr] = ref_time;
 		
 		if (N < wgid+1) {
@@ -96,6 +128,9 @@ void access(uint64_t addr, uint64_t wgid) {
 		}
 
 	} else {
+
+		intraRT_cal(addr, ref_time);
+
 		if (f[wgid].find(addr) == f[wgid].end()) {
 			f[wgid][addr] = ref_time;
 		} else {
@@ -133,14 +168,71 @@ double averagedCount(int n, int m, int i) {
 	return res / (n-i);
 }
 
+double averagedFCount(int n, int m, int i) {
+
+	double res = 1;
+	for (int j = 0; j < i; j++) {
+		res *= double (m-j) / (n-j);
+	}
+	return res / (n-i);
+}
+
+void fRT_cal(uint64_t addr, uint64_t avgL, uint64_t m) {
+
+	for (std::unordered_map<uint64_t, std::unordered_map<uint64_t, uint64_t> >::iterator fit = f.begin(), efit = f.end(); fit != efit; ++ fit) {
+		if (fit->second.find(addr) != fit->second.end()) {
+			for (int i = 0; i <= m; i++) {
+				uint64_t tmpRT = fit->second[addr] + i * avgL;
+				if (fRT.find(tmpRT) != fRT.end()) {
+					fRT[tmpRT] += averagedFCount(N, m, i);
+				} else {
+					fRT[tmpRT] = averagedFCount(N, m, i);
+				}
+			}
+		}
+	}	
+
+	return;
+}
+
+double averagedECount(int n, int m, int i) {
+	
+	double res = 1;
+    for (int j = 0; j < i; j++) {
+        res *= double (m-j) / (n-j);
+    }
+    return res / (n-i);
+}
+
+void eRT_cal(uint64_t addr, uint64_t avgL, uint64_t m) {
+	
+	for (std::unordered_map<uint64_t, std::unordered_map<uint64_t, uint64_t> >::iterator eit = e.begin(), eeit = e.end(); eit != eeit; ++ eit) {
+		if (eit->second.find(addr) != eit->second.end()) {
+			for (int i = 0; i <= m; i++) {
+				uint64_t tmpRT = eit->second[addr] + i * avgL;
+				if (eRT.find(tmpRT) != eRT.end()) {
+					eRT[tmpRT] += averagedECount(N, m, i);
+				} else {
+					eRT[tmpRT] = averagedECount(N, m, i);
+				}
+			}
+		}
+	}	
+
+	return;
+}
+
 
 void init_aeol() {
 	/* finilize exit time calculation for the last work group */
+	cout << "Number of work groups " << N << endl;
+
 	cout << "Start to finilize" << endl;
-	for (std::map<uint64_t, uint64_t>::iterator it = prevs.begin(), eit = prevs.end(); it != eit; ++it) {
+	for (std::unordered_map<uint64_t, uint64_t>::iterator it = prevs.begin(), eit = prevs.end(); it != eit; ++it) {
 		e[N-1][it->first] = ref_time - it->second + 1;
 	}
 	L[N-1] = ref_time;
+	cout << "End finilizing" << endl;
 
 #ifdef ADDRESS_SAMPLING
 	/* Address Sampling */
@@ -149,7 +241,11 @@ void init_aeol() {
 	
 #ifdef CLASSIFICATION
 
+	cout << "Start to classify" << endl;
 	for (std::set<uint64_t>::iterator it = D.begin(), endit = D.end(); it != endit; ++it) {
+		
+		//cout << *it << endl;
+
 		std::string tmp = "";
 		for (int i = 0; i < N; i++) {
 			if (f[i].find(*it)  == f[i].end()) {
@@ -180,8 +276,9 @@ void init_aeol() {
 		}	
 	
 		uint64_t id = CID[tmp];	
-		for (std::map<uint64_t, std::map<uint64_t, uint64_t> >::iterator fit = f.begin(), efit = f.end(); fit != efit; ++ fit) {
-			for (std::map<uint64_t, std::map<uint64_t, uint64_t> >::iterator eit = e.begin(), eeit = e.end(); eit != eeit; ++ eit) {
+		//cout << id << endl;
+		for (std::unordered_map<uint64_t, std::unordered_map<uint64_t, uint64_t> >::iterator fit = f.begin(), efit = f.end(); fit != efit; ++ fit) {
+			for (std::unordered_map<uint64_t, std::unordered_map<uint64_t, uint64_t> >::iterator eit = e.begin(), eeit = e.end(); eit != eeit; ++ eit) {
 				if (fit->first == eit->first) {
 					continue;
 				}
@@ -191,7 +288,8 @@ void init_aeol() {
 				if (eit->second.find(*it) == eit->second.end()) {
 					continue;
 				}
-				uint64_t rtTmp = valueToBin(fit->second[*it] + eit->second[*it]);
+				//uint64_t rtTmp = valueToBin(fit->second[*it] + eit->second[*it]);
+				uint64_t rtTmp = fit->second[*it] + eit->second[*it];
 				if (CEF[id].find(rtTmp) != CEF[id].end()) {
 					CEF[id][rtTmp] ++;
 				} else {
@@ -200,7 +298,11 @@ void init_aeol() {
 				}
 			}
 		}
+
+		fRT_cal(*it, CaveL[id], CM[id]);
+		eRT_cal(*it, CaveL[id], CM[id]);
 	}
+	cout << "End classify" << endl;
 
 #else
 	/* init D, F, E, M*/
@@ -224,7 +326,7 @@ void init_aeol() {
 		//cout << "start F" << endl;
 
 		F[*it] = new std::vector<uint64_t>();
-		for (std::map<uint64_t, std::map<uint64_t, uint64_t> >::iterator fit = f.begin(), efit = f.end(); fit != efit; ++ fit) {
+		for (std::unordered_map<uint64_t, std::unordered_map<uint64_t, uint64_t> >::iterator fit = f.begin(), efit = f.end(); fit != efit; ++ fit) {
 			if (fit->second.find(*it) != fit->second.end()) {
 				F[*it]->push_back(fit->second[*it]);
 			}
@@ -233,7 +335,7 @@ void init_aeol() {
 		//cout << "start E" << endl;
 
 		E[*it] = new std::vector<uint64_t>();
-		for (std::map<uint64_t, std::map<uint64_t, uint64_t> >::iterator eit = e.begin(), eeit = e.end(); eit != eeit; ++ eit) {
+		for (std::unordered_map<uint64_t, std::unordered_map<uint64_t, uint64_t> >::iterator eit = e.begin(), eeit = e.end(); eit != eeit; ++ eit) {
 			//cout << eit->first << " " << endl;
 			if (eit->second.find(*it) != eit->second.end()) {
 				E[*it]->push_back(eit->second[*it]);
@@ -282,7 +384,7 @@ void alg3() {
 
 	//for (std::set<std::bitset<N>>::iterator it = C.begin(), endit = C.end(); it != endit; ++it) {
 	for (uint64_t c = 0; c < cid; c++) {
-		for (std::map<uint64_t, uint64_t>::iterator cefit = CEF[c].begin(), ecefit = CEF[c].end(); cefit != ecefit; ++cefit) {
+		for (std::unordered_map<uint64_t, uint64_t>::iterator cefit = CEF[c].begin(), ecefit = CEF[c].end(); cefit != ecefit; ++cefit) {
 			for (int i = 0; i <= CM[c]; i++) {
 				uint64_t tmpRT = cefit->first + CaveL[c] * i - 1;
 				if (interRT.find(tmpRT) != interRT.end()) {
@@ -308,25 +410,149 @@ void alg3() {
 	return;
 }
 
+void mergeRT() {
+    cout << "start to Merge RT" << endl;
+    for (std::unordered_map<uint64_t, double>::iterator it = fRT.begin(), eit = fRT.end(); it != eit; ++it) {
+        if (RT.find(it->first) != RT.end()) {
+            RT[it->first] += it->second;
+        } else {
+            RT[it->first] = it->second;
+        }
+    }
+
+    for (std::unordered_map<uint64_t, double>::iterator it = eRT.begin(), eit = eRT.end(); it != eit; ++it) {
+        if (RT.find(it->first) != RT.end()) {
+            RT[it->first] += it->second;
+        } else {
+            RT[it->first] = it->second;
+        }
+    }
+
+    for (std::unordered_map<uint64_t, uint64_t>::iterator it = intraRT.begin(), eit = intraRT.end(); it != eit; ++it) {
+        if (RT.find(it->first) != RT.end()) {
+            RT[it->first] += it->second;
+        } else {
+            RT[it->first] = it->second;
+        }
+    }
+
+    for (std::unordered_map<uint64_t, double>::iterator it = interRT.begin(), eit = interRT.end(); it != eit; ++it) {
+        if (RT.find(it->first) != RT.end()) {
+            RT[it->first] += it->second;
+        } else {
+            RT[it->first] = it->second;
+        }
+    }
+    cout << "end merge" << endl;
+
+    return;
+
+}
+
+void RTtoFP() {
+
+	cout << "Cal fp" << endl;
+	uint64_t m = D.size();
+	//cout << "m " << m << endl;
+	//cout << "trace len " << trace_len << endl;
+	//for (int i = 1; i <= trace_len; i++) {
+	int i = 1;
+	while (i < trace_len) {	
+		
+		double tmp = m * (trace_len - i + 1);
+		for (std::map<uint64_t, double>::iterator it = RT.begin(), eit = RT.end(); it != eit; ++it) {
+			if (it->first > i) {
+				tmp -= it->second * (it->first - i);
+			}
+		}
+		FP[i] = tmp / (trace_len - i + 1); 
+	
+		if (i < ((uint64_t)1 << THRESHOLD)) {
+            i++;
+        } else {
+            i += i / BLOCK_NUM;
+        }
+	}
+
+	return;
+}
+
 void aeol() {
 
 	init_aeol();
 
 #ifdef CLASSIFICATION
+	cout << "Start alg3" << endl;
 	alg3();
 #else
+	cout << "Start alg2" << endl;
 	alg2();
 #endif	
+
+	mergeRT();
+	
+	RTtoFP();
 
 	return;
 }
 
+void dumpFP() {
+	for (std::map<uint64_t, double>::iterator it = FP.begin(), eit = FP.end(); it != eit; ++it) {
+		cout << it->first << " " << it->second << endl;
+	}
+
+	return;
+}
+
+void dumpRT() {
+	for (std::map<uint64_t, double>::iterator it = RT.begin(), eit = RT.end(); it != eit; ++it) {
+		cout << it->first << " " << it->second << endl;
+	}
+	return;
+}
+
+void dumpfRT() {
+	for (std::unordered_map<uint64_t, double>::iterator it = fRT.begin(), eit = fRT.end(); it != eit; ++it) {
+		cout << it->first << " " << it->second << endl;
+	}
+	return;
+}
+
+void dumpeRT() {
+	for (std::unordered_map<uint64_t, double>::iterator it = eRT.begin(), eit = eRT.end(); it != eit; ++it) {
+		cout << it->first << " " << it->second << endl;
+	}
+	return;
+}
+
+void dumpIntraRT() {
+
+	for (std::unordered_map<uint64_t, uint64_t>::iterator it = intraRT.begin(), eit = intraRT.end(); it != eit; ++it) {
+		cout << it->first << " " << it->second << endl;
+	}
+	return;
+}
+
 void dumpInterRT() {
-	for (std::map<uint64_t, double>::iterator it = interRT.begin(), eit = interRT.end(); it != eit; ++it) {
+	for (std::unordered_map<uint64_t, double>::iterator it = interRT.begin(), eit = interRT.end(); it != eit; ++it) {
 		if (it->second != 0) {
 			cout << it->first << "	" << it->second << endl;
 		}
 	}
+	return;
+}
+
+void dumpAllRT() {
+	
+	cout << "dump inter RT" << endl;
+	dumpInterRT();
+	cout << "dump intra RT" << endl;
+	dumpIntraRT();
+	cout << "dump fRT" << endl;
+	dumpfRT();
+	cout << "dump eRT" << endl;
+	dumpeRT();
+
 	return;
 }
 
@@ -339,9 +565,9 @@ void dumpD() {
 }
 
 void dumpf() {
-	for (std::map<uint64_t, std::map<uint64_t, uint64_t> >::iterator fit = f.begin(), efit = f.end(); fit != efit; ++ fit) {
+	for (std::unordered_map<uint64_t, std::unordered_map<uint64_t, uint64_t> >::iterator fit = f.begin(), efit = f.end(); fit != efit; ++ fit) {
 		cout << "work group " << fit->first << endl;
-		for (std::map<uint64_t, uint64_t>::iterator jt = fit->second.begin(), ejt = fit->second.end(); jt != ejt; ++jt) {
+		for (std::unordered_map<uint64_t, uint64_t>::iterator jt = fit->second.begin(), ejt = fit->second.end(); jt != ejt; ++jt) {
 			cout << "  d " << jt->first << " f " << jt->second << endl;
 		}
 	}
@@ -349,9 +575,9 @@ void dumpf() {
 }
 
 void dumpe() {
-	for (std::map<uint64_t, std::map<uint64_t, uint64_t> >::iterator eit = e.begin(), eeit = e.end(); eit != eeit; ++ eit) {
+	for (std::unordered_map<uint64_t, std::unordered_map<uint64_t, uint64_t> >::iterator eit = e.begin(), eeit = e.end(); eit != eeit; ++ eit) {
 		cout << "work group " << eit->first << endl;
-		for (std::map<uint64_t, uint64_t>::iterator jt = eit->second.begin(), ejt = eit->second.end(); jt != ejt; ++jt) {
+		for (std::unordered_map<uint64_t, uint64_t>::iterator jt = eit->second.begin(), ejt = eit->second.end(); jt != ejt; ++jt) {
 			cout << "  d " << jt->first << " e " << jt->second << endl;
 		}
 	}
@@ -359,7 +585,7 @@ void dumpe() {
 }
 
 void dumpL() {
-	for (std::map<uint64_t, uint64_t>::iterator it = L.begin(), eit = L.end(); it != eit; ++it) {
+	for (std::unordered_map<uint64_t, uint64_t>::iterator it = L.begin(), eit = L.end(); it != eit; ++it) {
 		cout << " work group " << it->first << " l " << it->second << endl; 
 	}		
 	return;
@@ -367,7 +593,7 @@ void dumpL() {
 
 void dumpC() {
 	cout << "Total number of classes " << cid << endl;
-	for (std::map<std::string, uint64_t>::iterator it = CID.begin(), eit = CID.end(); it != eit; ++it) {
+	for (std::unordered_map<std::string, uint64_t>::iterator it = CID.begin(), eit = CID.end(); it != eit; ++it) {
 		cout << it-> first << endl;
 	}
 	return;

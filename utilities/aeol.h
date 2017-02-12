ea37797094dd603a6474aa8f313009ecda9a5bc3
Dong Chen <dchen39@cycle3.cs.rochester.edu>
2017-02-12 17:31:22 -0500

    add AET

--- utilities/aeol.h
+++ utilities/aeol.h
@@ -49,6 +49,9 @@ std::map<uint64_t, double> RT;
 /* FP */
 std::map<uint64_t, double> FP;
 
+/* MR */
+std::map<uint64_t, double> MR;
+
 #define CLASSIFICATION
 
 //#define ADDRESS_SAMPLING 
@@ -477,6 +480,56 @@ void RTtoFP() {
 	return;
 }
 
+void RTtoMR_AET() {
+
+	cout << "Start convert RT to MR" << endl;
+
+	std::map<uint64_t, double> P;
+	double total_num_RT = 0;
+	uint64_t max_RT = 0;
+
+	for (std::map<uint64_t, double>::reverse_iterator it = RT.rbegin(), eit = RT.rend(); it != eit; ++it) {
+		total_num_RT += it->second;
+		if (max_RT < it->first) {
+			max_RT = it->first;
+		}
+	}
+	double accumulate_num_RT = 0;
+	for (std::map<uint64_t, double>::reverse_iterator it = RT.rbegin(), eit = RT.rend(); it != eit; ++it) {
+		P[it->first] = accumulate_num_RT / total_num_RT;
+		//cout << accumulate_num_RT << " " << total_num_RT << endl;
+		accumulate_num_RT += it->second;
+		//cout << "P " << it->first << " " << P[it->first] << endl;
+	}
+
+	P[0] = 1;
+
+	double sum_P = 0;
+	uint64_t t = 0;
+	uint64_t prev_t = 0;
+	for (uint64_t c = 0; c <= max_RT; c++) {
+		while (sum_P < c && t <= max_RT) {
+			if (P.find(t) != P.end()) {
+				sum_P += P[t];
+				prev_t = t;
+			} else {
+				sum_P += P[prev_t];
+			}
+			t++;
+		}
+		/*
+		if (P.find(t) != P.end()) {
+			MR[c] = P[t];
+		} else {
+			MR[c] = P[prev_t];
+		}*/
+		MR[c] = P[prev_t];
+	}
+
+	return;
+}
+
+
 void aeol() {
 
 	init_aeol();
@@ -491,8 +544,17 @@ void aeol() {
 
 	mergeRT();
 	
-	RTtoFP();
+	//RTtoFP();
 
+	RTtoMR_AET();
+
+	return;
+}
+
+void dumpMR() {
+	for (std::map<uint64_t, double>::iterator it = MR.begin(), eit = MR.end(); it != eit; ++it) {
+		cout << it->first << " " << it->second << endl;
+	}
 	return;
 }
 

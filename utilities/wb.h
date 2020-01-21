8a497a3b98a5925ee08c8729223bfea072340c24
dongchen-coder <jameschennerd@gmail.com>
2020-01-21 18:58:42 +0800

    update wb.h

--- utilities/wb.h
+++ utilities/wb.h
@@ -51,41 +51,41 @@ inline uint64_t valueToBin(uint64_t value) {
 }
 
 int writeRTHisto(uint64_t addr, unsigned long ref_time, char wr) {
-    
-  if (prevs.find(addr) != prevs.end()) {
-    if (wr == 'W') {
+
+	if (prevs.find(addr) != prevs.end()) {
+		if (wr == 'W') {
 			wtmp[addr] = 0;
 		}
-  }
+	}
 
 	if (wr == 'W') {
 		if (wtmp.find(addr) != wtmp.end()) {
-	    uint64_t maxRT;
-	    if(wtmp[addr] > ref_time - prevs[addr]) {
-	        maxRT = wtmp[addr];	
-	    } else {
+			uint64_t maxRT;
+			if(wtmp[addr] > ref_time - prevs[addr]) {
+				maxRT = wtmp[addr];	
+			} else {
 				maxRT = ref_time - prevs[addr];
-	    }
-      if (wrt_histo.find(maxRT) != wrt_histo.end()) {
-      	wrt_histo[maxRT] += 1;
-                //wrt_histo[valueToBin(maxRT)] += 1;
-      } else {
-        wrt_histo[maxRT] = 1;
-                //wrt_histo[valueToBin(maxRT)] += 1;
-      }
-	    wtmp[addr] = 0;
-    } else {
-	    wtmp[addr] = 0;    
-    }	
-  } else {
-  	if (wtmp.find(addr) != wtmp.end()) {
-	    if (wtmp[addr] < ref_time - prevs[addr]) {
-	    	wtmp[addr] = ref_time - prevs[addr];
-	    }
+			}
+			if (wrt_histo.find(maxRT) != wrt_histo.end()) {
+				wrt_histo[maxRT] += 1;
+				//wrt_histo[valueToBin(maxRT)] += 1;
+			} else {
+				wrt_histo[maxRT] = 1;
+				//wrt_histo[valueToBin(maxRT)] += 1;
+			}
+			wtmp[addr] = 0;
+		} else {
+			wtmp[addr] = 0;	
+		}	
+	} else {
+		if (wtmp.find(addr) != wtmp.end()) {
+			if (wtmp[addr] < ref_time - prevs[addr]) {
+				wtmp[addr] = ref_time - prevs[addr];
+			}
 		}   
-  }
+	}
 	 
-  return 0;
+	return 0;
 }
 
 
@@ -93,20 +93,24 @@ int RTtoFP() {
 
 	ref_time+=1;
 
+	// accumulate last access to the reuse interval histogram
 	for (std::map<uint64_t,uint64_t>::iterator it=prevs.begin(); it!=prevs.end(); ++it) {
-    rt_cnt_histo[valueToBin(ref_time - it->second)] += 1;
+		rt_cnt_histo[valueToBin(ref_time - it->second)] += 1;
 		rt_value_histo[valueToBin(ref_time - it->second)] += ref_time - it->second; 
-  }
-	/*
+	}
+	
+	// excluding the first access times
+	/* 
 	for (std::map<uint64_t,uint64_t>::iterator it = first_access_time.begin(), eit = first_access_time.end(); it != eit; ++it) {
 		rt_cnt_histo[valueToBin(it->second)] --;
 		rt_value_histo[valueToBin(it->second)] -= it->second;
-	}*/
-	
-	for (uint64_t i = 1; i < (1 << THRESHOLD); i++) {	
-			w[i] = i;
 	}
-
+	*/
+	
+	// calculate the window sizes in log scale after THRESHOLD
+	for (uint64_t i = 1; i < (1 << THRESHOLD); i++) {	
+		w[i] = i;
+	}
 	for (uint64_t i = THRESHOLD; i < 64; i++) {
 		uint64_t base = (uint64_t)1 << i;
 		for (uint64_t j = 0; j < BLOCK_NUM; j++) {
@@ -114,10 +118,8 @@ int RTtoFP() {
 		}
 	} 
 
+	// calculate footprint
 	map<uint64_t, uint64_t> sum;
-	
-//	cout << "here" << endl;
-
 	for (std::map<uint64_t, uint64_t>::iterator it=w.begin(); it!= w.end(); ++it) {
 		uint64_t wi = it->second;
 		for (std::map<uint64_t, uint64_t>::iterator jt=it; jt!= w.end(); ++jt) {
@@ -125,9 +127,6 @@ int RTtoFP() {
 			sum[wi] += rt_value_histo[wj] - rt_cnt_histo[wj] *  wi;
 		} 
 	}
-	
-//	cout << "here" << endl;
-
 	for (std::map<uint64_t, uint64_t>::iterator it=w.begin(); it!= w.end(); ++it) {
 		uint64_t wi = it->second;
 		if (ref_time > wi)
@@ -148,29 +147,30 @@ int RTHistoFirstAccessTimeFilter(uint64_t addr, unsigned long ref_time) {
 
 int RTHisto(uint64_t addr, unsigned long ref_time) {
 
-    if (prevs.find(addr) != prevs.end()) {
-        uint64_t rt = ref_time - prevs[addr];
+	if (prevs.find(addr) != prevs.end()) {
+		uint64_t rt = ref_time - prevs[addr];
 				rt = valueToBin(rt);
-    	if (rt_cnt_histo.find(rt) == rt_cnt_histo.end()) {
-        rt_cnt_histo[rt] = 1;
-	      rt_value_histo[rt] = ref_time - prevs[addr];
-    	} else {
-      	rt_cnt_histo[rt] += 1;
-	    	rt_value_histo[rt] += ref_time - prevs[addr];
-     	}
-    } else {
-        if (rt_cnt_histo.find(valueToBin(ref_time)) != rt_cnt_histo.end()) {
-          rt_cnt_histo[valueToBin(ref_time)] += 1;
+		if (rt_cnt_histo.find(rt) == rt_cnt_histo.end()) {
+		rt_cnt_histo[rt] = 1;
+		  rt_value_histo[rt] = ref_time - prevs[addr];
+		} else {
+	  	rt_cnt_histo[rt] += 1;
+			rt_value_histo[rt] += ref_time - prevs[addr];
+	 	}
+	} else {
+		if (rt_cnt_histo.find(valueToBin(ref_time)) != rt_cnt_histo.end()) {
+		  rt_cnt_histo[valueToBin(ref_time)] += 1;
 					rt_value_histo[valueToBin(ref_time)] += ref_time;
-        } else {
-          rt_cnt_histo[valueToBin(ref_time)] = 1;
+		} else {
+		  rt_cnt_histo[valueToBin(ref_time)] = 1;
 					rt_value_histo[valueToBin(ref_time)] = ref_time;
-        }
-    }
-    prevs[addr] = ref_time;
-    return 0;
+		}
+	}
+	prevs[addr] = ref_time;
+	return 0;
 }
 
+// calculate miss ratio from footprint for specific cache size
 double FPtoMR(int cacheSize) {
 
 	/*
@@ -196,13 +196,13 @@ double FPtoMR(int cacheSize) {
 		prev_fp = it->second;
 	}
 
-    return 0;
+	return 0;
 }
 
-
+// calculate write backs
 int WBHisto(void) {
 
-	cout << "start to calculate wb histo" << endl;
+	cout << "Start to calculate write reuse intervals for last writes before flush" << endl;
 	for (std::map<uint64_t,uint64_t>::iterator it=wtmp.begin(); it!=wtmp.end(); ++it) {
 		uint64_t maxRT;
 		if (it->second > ref_time - prevs[it->first]) {
@@ -218,8 +218,7 @@ int WBHisto(void) {
 		}
 	}
 
-	cout << "Finish wrt_histo" << endl;
-
+	cout << "Finish calculating write reuse intervals" << endl;
 	for (std::map<uint64_t, uint64_t>::iterator it = w.begin(); it != w.end(); ++it) {
 		uint64_t i = it->second;
 		uint64_t filltime = 0;
@@ -253,7 +252,7 @@ int WBHisto(void) {
 				break;
 			}
 		}	
-     
+	 
 		for (kt = wrt_histo.begin(); kt != wrt_histo.end(); ++kt) {
 			if (kt->first == j) {
 				break;
@@ -273,7 +272,6 @@ int WBHisto(void) {
 		}
 	}
 
-	
 	return 0;
 }
 
@@ -300,31 +298,32 @@ void dumpMR() {
 void access(uint64_t addr, uint64_t wid) {
 	ref_time++;
 	addr = addr * DS / CLS;
-    RTHisto(addr, ref_time);
-    return;
+	RTHisto(addr, ref_time);
+	return;
 }
 
 void access(uint64_t addr, char wr) {
-    
-    ref_time++;
+	
+	ref_time++;
 	if (wr == 'W') {
 		totalWrites += 1;
 		if (totalWrites == 0) {
 			cout << "overflow" << endl;
 		}
 	}
-    //writeRTHisto(addr, ref_time, wr);
-	//RTHistoFirstAccessTimeFilter(addr, ref_time);
-    
 	addr = addr * DS / CLS;
+	
+	writeRTHisto(addr, ref_time, wr);
+	//RTHistoFirstAccessTimeFilter(addr, ref_time);
+	
 	RTHisto(addr, ref_time);
 	return;
 }
 
 void dumpFP() {
 	for (std::map<uint64_t, double>::iterator it=footprint_avg.begin(); it!=footprint_avg.end(); ++it) {
-                cout << "win " << it->first << " footprint " << it->second << endl;
-    }
+				cout << "win " << it->first << " footprint " << it->second << endl;
+	}
 	return;
 }
 
@@ -351,17 +350,17 @@ int dump(char* fileName) {
 	else
 		fs.open (fileName_s.c_str(),  std::fstream::out | std::fstream::app);
 
-        for (std::map<uint64_t, double>::iterator it=footprint_avg.begin(); it!=footprint_avg.end(); ++it) {
-                fs << "win " << it->first << " footprint " << it->second << endl;
-        }
+		for (std::map<uint64_t, double>::iterator it=footprint_avg.begin(); it!=footprint_avg.end(); ++it) {
+				fs << "win " << it->first << " footprint " << it->second << endl;
+		}
 
 	for (std::map<uint64_t, uint64_t>::iterator it=wb.begin(); it!=wb.end(); ++it) {
 		fs << "cache size " << it->first << " wb " << it->second << endl;
 	}
 	
 	for (std::map<uint64_t, uint64_t>::iterator it=twb.begin(); it!=twb.end(); ++it) {
-                fs << "cache size " << it->first << " twb " << it->second << endl;
-        }
+				fs << "cache size " << it->first << " twb " << it->second << endl;
+		}
 
 	fs.close();
 
@@ -392,11 +391,11 @@ void reset() {
 int main() {
 
 	for (uint64_t i = 0; i < 4; i++) {
-                Access(1, 'R');
-		Access(2, 'W');
-		Access(3, 'R');
-		Access(4, 'R');
-        }	
+		access(1, 'R');
+		access(2, 'W');
+		access(3, 'R');
+		access(4, 'R');
+	}
 
 	cout << "finish access " << endl;
 	
